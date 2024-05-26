1d8c4516f297ca074577d6de4d0f1454da1bb4ac
Rayuga Nub <phoeniximmortal12@gmail.com>
2024-05-26 15:43:23 +0545

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id:1,
+        rating:10,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim ad exercitationem possimus sunt, earum vel omnis, aperiam natus, necessitatibus molestias? Eum quas harum mollitia voluptas sit, vero eveniet maiores."
+      },
+      {
+        id:2,
+        rating:9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim ad exercitationem possimus sunt, earum vel omnis, aperiam natus, necessitatibus molestias? Eum quas harum mollitia voluptas sit, vero eveniet maiores."
+      },
+      {
+        id:3,
+        rating:8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim ad exercitationem possimus sunt, earum vel omnis, aperiam natus, necessitatibus molestias? Eum quas harum mollitia voluptas sit, vero eveniet maiores."
+      },
+]
+
+export default FeedbackData
